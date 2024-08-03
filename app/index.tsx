668d82eaf46fe596fd646962d7dbcf7b7dd66b76
enzoalencar <enzo14alencar@gmail.com>
2024-08-03 20:40:57 -0300

    feat: add markers on map

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
-import { useCallback, useEffect, useRef } from "react";
-import MapView from "react-native-maps";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { MapView, Marker } from "react-native-maps";
 
 import * as Location from "expo-location";
 
@@ -8,8 +8,9 @@ import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 
 import { View, Button } from "tamagui";
 
-export default function HomeScreen() {
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 
+export default function HomeScreen() {
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -26,6 +27,19 @@ export default function HomeScreen() {
     console.log("handleSheetChanges", index);
   }, []);
 
+  const [markers, setMarkers] = useState([
+    {
+      title: "hello",
+      description: "world",
+      latlng: { latitude: -23.5105, longitude: -47.60213 },
+    },
+    {
+      title: "hello2",
+      description: "world",
+      latlng: { latitude: -23.5105, longitude: -47.61513 },
+    },
+  ]);
+
   return (
     <View
       style={{
@@ -34,13 +48,31 @@ export default function HomeScreen() {
     >
       <MapView
         style={styles.map}
+        mapType="standard"
+        userInterfaceStyle="light"
         region={{
           latitude: -23.5505,
           longitude: -47.60013,
           latitudeDelta: 0.08,
           longitudeDelta: 0.08,
         }}
-      />
+      >
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+          >
+            <FontAwesome6
+              name="house-flood-water"
+              size={26}
+              color="dodgerblue"
+            />
+          </Marker>
+        ))}
+      </MapView>
+
       <BottomSheet
         ref={bottomSheetRef}
         onChange={handleSheetChanges}
