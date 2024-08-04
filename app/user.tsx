cc9a01fa98f2eb221f4414e5dd87187adc1aa1ae
enzoalencar <enzo14alencar@gmail.com>
2024-08-04 11:21:49 -0300

    feat: add user profile page

--- app/user.tsx
+++ app/user.tsx
@@ -1,5 +1,74 @@
-import { Text } from "react-native";
+import type { SizeTokens } from "tamagui";
+import {
+  Label,
+  Separator,
+  Switch,
+  View,
+  XStack,
+  YStack,
+  Avatar,
+  Text,
+} from "tamagui";
 
 export default function UserPage() {
-  return <Text>teste</Text>;
+  return (
+    <View paddingTop="$14">
+      <View justifyContent="center" alignItems="center" gap="$2">
+        <Avatar circular size="$6">
+          <Avatar.Image src="http://picsum.photos/200/300" />
+          <Avatar.Fallback bc="black" />
+        </Avatar>
+        <Text fontSize="$5" fontWeight="bold">
+          Name
+        </Text>
+      </View>
+
+      <View gap="$1" paddingBottom="$4" paddingTop="$6">
+        <Label unstyled color={"white"} fontSize={"$4"} fontWeight="bold">
+          E-mail
+        </Label>
+        <Text fontSize={"$3"}>Nome@gmail.com</Text>
+      </View>
+
+      <YStack width={400} alignItems="flex-start" gap="$3">
+        <XStack gap="$4" $xs={{ flexDirection: "column" }}>
+          <View gap="$1.5">
+            <Label unstyled color={"white"} fontSize={"$4"} fontWeight="bold">
+              Notificar sensores favoritados?
+            </Label>
+            <SwitchWithLabel size="$2" defaultChecked />
+          </View>
+
+          <View gap="$1.5">
+            <Label unstyled color={"white"} fontSize={"$4"} fontWeight="bold">
+              Notificar sensores novos perto da sua área?
+            </Label>
+            <SwitchWithLabel size="$2" />
+          </View>
+        </XStack>
+      </YStack>
+    </View>
+  );
+
+  function SwitchWithLabel(props: {
+    size: SizeTokens;
+    defaultChecked?: boolean;
+  }) {
+    return (
+      <XStack width={200} alignItems="center" gap="$4">
+        <Switch size={props.size} defaultChecked={props.defaultChecked}>
+          <Switch.Thumb animation="quicker" />
+        </Switch>
+        <Separator minHeight={20} vertical />
+        <Label
+          padding="$0"
+          minWidth={90}
+          justifyContent="flex-end"
+          size={props.size}
+        >
+          {props.defaultChecked ? "Sim" : "Não"}
+        </Label>
+      </XStack>
+    );
+  }
 }
